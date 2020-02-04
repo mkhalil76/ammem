78e29899731dc8926386cd23330d67c7f3fe9bd3
Mohammed Khalil <mkhalilkhalil67@gmail.com>
2020-02-04 10:38:24 +0200

    cros origin

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@
 
 Route::group(['prefix' => version_api(), 'namespace' => namespace_api(), 'as' => 'api.'], function () {
 
-    Route::middleware('corss')->group(['prefix' => 'auth'], function () {
+    Route::group(['middleware' => ['web'],'prefix' => 'auth'], function () {
         Route::post('user', 'UserController@PostNewUser');
     });
 	Route::post('reset-user', 'UserController@Reset');
